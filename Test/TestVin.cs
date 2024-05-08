b6a9c21aa4db4d27f91da29b20ce8fdfef527331
Anton Yaroshenko <kpakadijl@hotmail.com>
2024-05-08 12:01:07 +0300

    vin decoding tests

--- Test/TestVin.cs
+++ Test/TestVin.cs
@@ -18,90 +18,122 @@
 using DaleNewman;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Test {
+namespace Test
+{
 
     [TestClass]
-    public class TestVin {
+    public class TestVin
+    {
 
         [TestMethod]
-        public void Test17Ones() {
+        public void Test17Ones()
+        {
             Assert.IsTrue(Vin.IsValid("11111111111111111"), "Is true because all ones pass.");
         }
 
         [TestMethod]
-        public void Test16Ones() {
+        public void Test16Ones()
+        {
             Assert.IsFalse(Vin.IsValid("1111111111111111"), "Is false because it's 16 characters.");
         }
 
         [TestMethod]
-        public void Test18Ones() {
+        public void Test18Ones()
+        {
             Assert.IsFalse(Vin.IsValid("111111111111111111"), "Is false because it's 18 characters.");
         }
 
         [TestMethod]
-        public void TestOneThree() {
+        public void TestOneThree()
+        {
             Assert.IsFalse(Vin.IsValid("11111111111111113"), "Is false because I changed the last 1 to a 3.");
         }
 
         [TestMethod]
-        public void TestRealVin() {
+        public void TestRealVin()
+        {
             Assert.IsTrue(Vin.IsValid("1FTKR1ADXAPA11957"), "Is true because this is a real VIN (I hope)");
         }
 
+        //[TestMethod]
+        //public void TestNissanVin()
+        //{
+        //    var decoder = new VinDecoder();
+        //    var yeaar = decoder.GetVinYear("JN1TBNT30U0127688");
+
+        //    var manufacturer = Vin.GetWorldManufacturer("JN1TBNT30U0127688");
+        //    var year = Vin.GetModelYear("JN1TBNT30U0127688");
+        //    Assert.Equals();
+        //}
+
         [TestMethod]
-        public void TestRealVinWithPInsteadOfF() {
+        [DataRow("JNKBV61F77M804426", 2007, "Infiniti", "G", "35x")]
+        public void TestValidVins(string vin, int year, string brand, string model, string modification)
+        {
+            var parsedYear = Vin.GetVinYear(vin);
+            var manufacturer = Vin.GetWorldManufacturer(vin);
+            Assert.AreEqual(year, parsedYear);
+            Assert.AreEqual(brand, manufacturer);
+        }
+
+        [TestMethod]
+        public void TestRealVinWithPInsteadOfF()
+        {
             Assert.IsFalse(Vin.IsValid("1PTKR1ADXAPA11957"), "Is false because I replaced F with P.");
         }
 
         [TestMethod]
-        public void TestInvalidWmi() {
+        public void TestInvalidWmi()
+        {
             Assert.AreEqual(string.Empty, Vin.GetWorldManufacturer("111"));
         }
 
         [TestMethod]
-        public void TestFord() {
+        public void TestFord()
+        {
             Assert.AreEqual("Ford Motor Company", Vin.GetWorldManufacturer("1FD"));
         }
 
         [TestMethod]
-        public void TestInternational() {
-            Assert.AreEqual("International", Vin.GetWorldManufacturer("1HT"));
+        public void TestInternational()
+        {
+            Assert.AreEqual("International Trucks & Caterpillar Trucks & Chevrolet Silverado 4500HD, 5500HD, 6500HD incomplete vehicle", Vin.GetWorldManufacturer("1HT"));
         }
 
         [TestMethod]
-        public void TestToyotaMatchingFirstTwo() {
-            Assert.AreEqual("Toyota USA - trucks", Vin.GetWorldManufacturer("5TG"));
+        public void TestToyotaMatchingFirstTwo()
+        {
+            Assert.AreEqual("Toyota truck made by TMMI", Vin.GetWorldManufacturer("5TG"));
         }
 
         [TestMethod]
-        public void TestGetYear() {
+        public void TestGetYear()
+        {
             Assert.AreEqual(2001, Vin.GetModelYear("1111111111"));
         }
 
         [TestMethod]
-        public void TestGetYearForA() {
+        public void TestGetYearForA()
+        {
             Assert.AreEqual(2010, Vin.GetModelYear("111111111A"));
         }
 
         [TestMethod]
-        public void TestGetYearForAForceDown() {
+        public void TestGetYearForAForceDown()
+        {
             Assert.AreEqual(1980, Vin.GetModelYear("111111111A", 1980));
         }
 
         [TestMethod]
-        public void TestGetYearForK() {
-            Assert.AreEqual(1989, Vin.GetModelYear("111111111K"), "This should be 1989 because 2019 vehicles do not exist yet (given now is 2016).");
+        public void TestGetYearForK()
+        {
+            Assert.AreEqual(2019, Vin.GetModelYear("111111111K"), "This should be 1989 because 2019 vehicles do not exist yet (given now is 2016).");
         }
 
         [TestMethod]
-        public void TestGetYearForD() {
+        public void TestGetYearForD()
+        {
             Assert.AreEqual(2013, Vin.GetModelYear("111111111D"), "This should be 2013 because 1983 is less likely (given now is 2016).");
-        }
-
-
-        [TestMethod]
-        public void TestGetYearForDChar() {
-            Assert.AreEqual(2013, Vin.GetModelYear('D'), "This should be 2013 because 1983 is less likely (given now is 2016).");
         }
     }
 }
---
+++ Vin/CodeValue.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DaleNewman
+{
+    public record CodeValue
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
